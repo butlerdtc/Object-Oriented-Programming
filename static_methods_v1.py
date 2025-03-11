01716660c526df5d630a5f22f4e054defa11178f
butlerdtc <butlerr2@middleton.school.nz>
2025-03-11 17:04:53 +1300

    Update

--- static_methods_v1.py
+++ static_methods_v1.py
@@ -1,71 +1,32 @@
-class AllStaff:
-    number_of_staff = 0
-    RETIREMENT_AGE = 65
+class GSTCases:
 
-    def __init__(self, name, age, id, birthdate, job):
-        self.name = name
-        self.age = age
-        self.id = id
-        self.birthdate = birthdate
-        self.job = job
+    @staticmethod
+    def calc_gst(i):
+        return i * 0.15
 
-        AllStaff.calc_number_of_staff()
+    @staticmethod
+    def add_gst(i):
+        return i + (i * 0.15)
 
+    @staticmethod
+    def gst_comp(i):
+        return i * (3 / 23)
 
-    def show(self):
-        print(f"Added {self.id} is {self.name} aged {self.age} being born "
-              f"{self.birthdate} employed as {self.job}")
+    @staticmethod
+    def gst_excl(i):
+        return i / 1.15
 
-    @classmethod
-    def calc_number_of_staff(cls):
-        cls.number_of_staff += 1
-
-    @classmethod
-    def total_staff(cls):
-        return cls.number_of_staff
+    @staticmethod
+    def print_stars():
+        return "*" * 100
 
 
-class Management(AllStaff):
-    def __init__(self, name, age, id, birthdate, job, car):
-        super().__init__(name, age, id, birthdate, job)
-        self.car = car
-    def show(self):
-        print(f"Added {self.id} is {self.name} aged {self.age} being born "
-              f"{self.birthdate} employed as {self.job} driving a {self.car}")
-
-class Clerical(AllStaff):
-    def __init__(self, name, age, id, birthdate, job, typing_speed):
-        super().__init__(name, age, id, birthdate, job)
-        self.typing_speed = typing_speed
-    def show(self):
-        print(f"Added {self.id} is {self.name} aged {self.age} being born "
-              f"{self.birthdate} employed as {self.job} with a typing speed of"
-              f" {self.typing_speed}")
-
-class Factory(AllStaff):
-    pass
-
-
-a = Management("Jenny", 22, "ID007", "20/10/2000",
-               "Managing Director", "Jaguar")
-print(f"Total number of staff is now {AllStaff.total_staff()}")
-a.show()
-print(f"NOTE: Only {AllStaff.RETIREMENT_AGE - a.age} years until {a.name} "
-      f"retires")
-print()
-
-b = Clerical("Tim", 17, "ID119", "01/01/2005",
-             "Secretary", 123)
-print(f"Total number of staff is now {AllStaff.total_staff()}")
-b.show()
-print(f"NOTE: Only {AllStaff.RETIREMENT_AGE - b.age} years until {b.name} "
-      f"retires")
-print()
-
-c = Factory("Jake", 16, "ID125", "17/08/2006",
-            "Labourer")
-print(f"Total number of staff is now {AllStaff.total_staff()}")
-c.show()
-print(f"NOTE: Only {AllStaff.RETIREMENT_AGE - c.age} years until {c.name} "
-      f"retires")
-print()
+# Main routine
+print(GSTCases.print_stars())
+print(f"GST on $100 is ${GSTCases.calc_gst(100)}")
+print(f"so adding GST to $100 is ${GSTCases.add_gst(100)}")
+print(GSTCases.print_stars())
+print(f"If the GST inclusive price is $10,350, the GST amount is $"
+      f"{GSTCases.gst_comp(10350)}")
+print(f"so the GST exclusive amount is ${GSTCases.gst_excl(10350)}")
+print(GSTCases.print_stars())
