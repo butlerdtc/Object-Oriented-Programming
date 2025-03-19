640363e27a372f0f7db43fe6f64458adbb0f0516
butlerdtc <butlerr2@middleton.school.nz>
2025-03-19 17:25:19 +1300

    Update Banking System.py

--- Banking System.py
+++ Banking System.py
@@ -12,30 +12,60 @@ class User:
         self.account_no = account_no
         userList.append(self)
 
+    @staticmethod
+    def print_stars():
+        return "*" * 40
 
-def generateUsers():
+
+    def display_info(self):
+        print(User.print_stars())
+        print(f"First Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Gender: {self.gender}")
+        print(f"Street Address: {self.street_address}")
+        print(f"City: {self.city}")
+        print(f"Email: {self.email}")
+        print(f"Credit Card Number: {self.cc_number}")
+        print(f"Credit Card Type: {self.cc_type}")
+        print(f"Balance: {self.balance}")
+        print(f"Account Number: {self.account_no}")
+
+
+def generate_users():
     import csv
     with open('bankUsers.csv', newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter=',', quotechar="'")
         for line in filereader:
             User(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], float(line[8]), line[9])
 
-def findUser():
-    # TO COMPLETE
+def find_user():
+    while True:
+        user_first = input("Enter the first name of the user to find: ").title()
+        user_last = input("Enter the last name of the user to find: ").title()
+        user_found = False
+        for users in userList:
+            if users.first_name == user_first:
+                if users.last_name == user_last:
+                    users.display_info()
+                    user_found = True
+                    break
+        if user_found:
+            break
+        else:
+            print("User not found")
 
-    True
     
 def overdrafts():
     # TO COMPLETE
     
     True
     
-def missingEmails():
+def missing_emails():
     # TO COMPLETE
 
     True
 
-def bankDetails():
+def bank_details():
     # TO COMPLETE
 
     True
@@ -46,30 +76,33 @@ def transfer():
     True
 
 userList = []          
-generateUsers()
+generate_users()
 
-userChoice = ""
-print("Welcome")
-
-while userChoice != "Q":
-    print("What function would you like to run?")
-    print("Type 1 to find a user")
-    print("Type 2 to print overdraft information")
-    print("Type 3 to print users with missing emails")
-    print("Type 4 to print bank details")
-    print("Type 5 to transfer money")
-    print("Type Q to quit")
-    userChoice = input("Enter choice: ")
-    print()
-    
-    if userChoice == "1":
-        findUser()
-    elif userChoice == "2":
-        overdrafts()
-    elif userChoice == "3":
-        missingEmails()
-    elif userChoice == "4":
-        bankDetails()
-    elif userChoice == "5":
-        transfer()      
-    print()
+# userChoice = ""
+# print("Welcome")
+#
+# while userChoice != "Q":
+#     print("What function would you like to run?")
+#     print("Type 1 to find a user")
+#     print("Type 2 to print overdraft information")
+#     print("Type 3 to print users with missing emails")
+#     print("Type 4 to print bank details")
+#     print("Type 5 to transfer money")
+#     print("Type Q to quit")
+#     userChoice = input("Enter choice: ")
+#     print()
+#
+#     if userChoice == "1":
+#         findUser()
+#     elif userChoice == "2":
+#         overdrafts()
+#     elif userChoice == "3":
+#         missingEmails()
+#     elif userChoice == "4":
+#         bankDetails()
+#     elif userChoice == "5":
+#         transfer()
+#     print()
+# for user in userList:
+#     user.display_info()
+find_user()
