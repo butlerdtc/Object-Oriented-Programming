c33a893823432173343c8e084092b642be4c4553
butlerdtc <butlerr2@middleton.school.nz>
2025-03-23 16:17:13 +1300

    Update Banking System.py

--- Banking System.py
+++ Banking System.py
@@ -12,6 +12,7 @@ class User:
         self.account_no = account_no
         userList.append(self)
 
+    # Static Method to print stars before printing user info
     @staticmethod
     def print_stars():
         return "*" * 40
@@ -28,6 +29,12 @@ class User:
         print(f"Credit Card Type: {self.cc_type}")
         print(f"Balance: {self.balance}")
         print(f"Account Number: {self.account_no}")
+
+    def add_balance(self, amount_to_add):
+        self.balance += amount_to_add
+
+    def remove_balance(self, amount_to_remove):
+        self.balance -= amount_to_remove
 
 
 def integer_checker(question):
@@ -73,7 +80,8 @@ def overdrafts():
         if user.balance < 0:
             count_users += 1
             count_balance += user.balance
-            print(f"Name: {user.first_name} {user.last_name}")
+            print(f"Name: {user.first_name} {user.last_name} | Balance: $"
+                  f"{user.balance}")
     edited_balance = count_balance * -1
     print(f"Total number of users with negative balances are {count_users}")
     print(f"Total amount due from these users is ${edited_balance:.2f}")
@@ -82,79 +90,133 @@ def overdrafts():
 def missing_emails():
     number_of_users = 0
     for user in userList:
-        if user.email != "":
+        if len(user.email) < 1:
             number_of_users += 1
             print(f"Name: {user.first_name} {user.last_name}")
     if number_of_users == 0:
         print("All users have emails")
+    else:
+        print(f"There are {number_of_users} users with no emails")
 
 
+# Function to print total users, total value and highest and lowest users
 def bank_details():
+    # Tracks number of users
     count = 0
+    # Tracks total balance of all users
     total_value = 0
+    # Tracks the highest of the user with the most
     highest_balance = 0
-    highest_user = ""
+    # Tracks the lowest balance of the user with the least
     lowest_balance = 0
+    # Initializes variables for highest/lowest users names
+    highest_user = ""
     lowest_user = ""
+    # Iterates through users in list
     for user in userList:
+        # Adds user to the count
         count += 1
+        # Adds user balance to the total
         total_value += user.balance
-        lowest_balance += user.balance
+        # If the users balance is higher than the current highest
         if user.balance > highest_balance:
+            # Makes this users balance the highest
             highest_balance = user.balance
-            highest_user = user.first_name + user.last_name
-    print(f"{highest_user}: ${highest_balance}")
+            # Sets the highest users name to the variable
+            highest_user = user.first_name + " " + user.last_name
+    # Iterates through users in list
+    for user in userList:
+        # If users balance is lower than the lowest currently
+        if user.balance < lowest_balance:
+            # Updates variables for new lowest user
+            lowest_balance = user.balance
+            lowest_user = user.first_name + " " + user.last_name
+
+    print(f"There are {count} users at this bank")
+    print(f"There is a total value of ${total_value}")
+    print(f"User with the highest balance is {highest_user} with a balance of "
+          f"${highest_balance}")
+    print(f"User with the lowest balance is {lowest_user} with a balance of "
+          f"${lowest_balance}")
 
 
+# Function to get the amount to transfer
 def transfer_amount(user):
     while True:
-        transfer_amount_ = integer_checker("How much would you like to "
+        # Checks the amount to transfer is a number
+        amount_to_transfer = integer_checker("How much would you like to "
                                            "transfer: ")
-        if transfer_amount_ <= 0:
+        # Resets loop if user tries to transfer 0 or less
+        if amount_to_transfer <= 0:
             print("Please enter an amount above $0")
             continue
-        elif transfer_amount_ <= user.balance:
+        # If amount is less (/equal) to balance and greater than 0, breaks loop
+        elif amount_to_transfer <= user.balance:
             break
         else:
             print(f"Please choose an amount from your remaining "
                   f"balance of ${user.balance}")
-    return transfer_amount_
+    return amount_to_transfer
 
 
+# Function to find the account to transfer money to
 def account_to_transfer(original_acc):
     while True:
-        account = input("Enter the number of the account to transfer"
-                                  " to: ")
+        account = input("Enter the number of the account to transfer to: ")
+        found = False  # Add a flag to track if the account was found
         for user in userList:
             if account == original_acc:
                 print("You cannot transfer to the same account")
-                continue
+                break  # Exit the loop since the account is invalid
             elif account == user.account_no:
                 return account, user.first_name, user.last_name, user
+        else: # This else statement is attached to the for loop. It executes
+            # when the for loop finishes normally (no break)
+            if not found and account != original_acc:
+                print("Enter an account in the system")
+                continue #continue to the next loop iteration.
 
 
+# Function to run the transfers
 def transfer():
-    account_no_input = input("Enter the number of the account to find: ")
+    # Get the number of the account to transfer from
+    account_no_input = input("Enter the number of your account to find: ")
     for user in userList:
+        # If the account number matches one in the user list
         if account_no_input == user.account_no:
+            # Prints details of the account
             print(f"Name: {user.first_name} {user.last_name}")
             print(f"Account balance: ${user.balance}")
+            # Runs function to get the amount to transfer
             amount_to_transfer = transfer_amount(user)
+            # Runs function to get details of account to transfer to
             transfer_account, transfer_first, transfer_last, transfer_user = (
                 account_to_transfer(user.account_no))
+            # Prints details of account to transfer to
             print(f"Confirm you want to transfer ${amount_to_transfer} to "
                   f"{transfer_first} {transfer_last} with the account number "
                   f"{transfer_account}")
+            # Asks user to confirm transfer
             confirm = input("Type 'Yes' to confirm the transfer: ").lower()
             if confirm == "yes":
-                user.balance -= amount_to_transfer
-                transfer_user.balance += amount_to_transfer
+                # Gets original balances
+                original_balance_from = user.balance
+                original_balance_to = transfer_user.balance
+                # Updates balances with class method
+                user.remove_balance(amount_to_transfer)
+                transfer_user.add_balance(amount_to_transfer)
+                print()
+                # Prints user details
                 print(f"Name: {user.first_name} {user.last_name}\nAccount "
-                      f"Number: {user.account_no}\nAccount Balance: ${user.
-                      balance}")
+                      f"Number: {user.account_no}\nOriginal Account Balance: "
+                      f"${original_balance_from}\nUpdated Account Balance: "
+                      f"${user.balance}")
                 print(f"Name: {transfer_first} {transfer_last}\nAccount "
-                      f"Number: {transfer_account}\nAccount Balance: ${
-                      transfer_user.balance}")
+                      f"Number: {transfer_account}\nOriginal Account Balance: "
+                      f"${original_balance_to}\nUpdated Account Balance: "
+                      f"${transfer_user.balance}")
+            else:
+                print("Transfer cancelled")
 
 
 # Main routine
@@ -162,17 +224,17 @@ userList = []
 generate_users()
 
 userChoice = ""
-print("Welcome")
+print("Welcome\n")
 
 while userChoice != "Q":
-    print("What function would you like to run?")
+    print("What function would you like to run?\n")
     print("Type 1 to find a user")
     print("Type 2 to print overdraft information")
     print("Type 3 to print users with missing emails")
     print("Type 4 to print bank details")
     print("Type 5 to transfer money")
     print("Type Q to quit")
-    userChoice = input("Enter choice: ")
+    userChoice = input("\nEnter choice: ")
     print()
 
     if userChoice == "1":
