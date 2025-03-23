270eee2f512af0e3a31dfdfeadc0e4b82829ff58
butlerdtc <butlerr2@middleton.school.nz>
2025-03-24 12:34:22 +1300

    Update 04_student_system_v1.py

--- 04_student_system_v1.py
+++ 04_student_system_v1.py
@@ -15,6 +15,19 @@ class Students:
               f"\nForm class: {self.form}\nSubjects: {self.subjects}\nGender: "
               f"{self.gender}\nEnrolled: {self.enrolled}")
 
+    def display_name(self):
+        print(f"Name: {self.name}")
+
+
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+        teacher_list.append(self)
+
+    def display_info(self):
+        print(f"Teacher Name: {self.name}\nSubject: {self.subject}")
+
 
 def integer_checker(question):
     error = "\nSorry, you must enter an integer\n"
@@ -25,6 +38,20 @@ def integer_checker(question):
             return number
         except ValueError:
             print(error)
+
+
+def get_classes():
+    class_list = []
+    for teacher in teacher_list:
+        class_list.append(teacher.subject)
+    return class_list
+
+
+def get_form_classes():
+    form_classes = []
+    for teacher in teacher_list:
+        form_classes.append(teacher.name.upper())
+    return form_classes
 
 
 def get_subjects(subject_options):
@@ -76,6 +103,11 @@ def print_student_details():
         student.display_info()
 
 
+def print_teacher_details():
+    for teacher in teacher_list:
+        teacher.display_info()
+
+
 def select_student_age():
     age_requirement = integer_checker("Please enter the minimum age to search "
                                       "for: ")
@@ -84,7 +116,7 @@ def select_student_age():
         if student.age >= age_requirement:
             count += 1
             student.display_info()
-    print(f"There are {count} students at {age_requirement} or older")
+    print(f"\nThere are {count} students at {age_requirement} or older")
 
 
 def count_students(subject_options):
@@ -97,7 +129,9 @@ def count_students(subject_options):
                 subjects_list = student.subjects.split(", ")
                 if class_to_search in subjects_list:
                     count += 1
-            return count
+            for teacher in teacher_list:
+                if teacher.subject == class_to_search:
+                    return count, teacher.name
         else:
             print("Please choose one of the classes")
 
@@ -117,6 +151,26 @@ def find_student():
         else:
             print("Student not found in system")
             break
+
+
+def print_gender():
+    while True:
+        chosen_gender = (input("Enter the gender of students to print: ")
+                         .title())
+        if chosen_gender == "Male":
+            choice = "Male"
+            break
+        elif chosen_gender == "Female":
+            choice = "Female"
+            break
+        else:
+            print("Please choose 'Male' or 'Female'")
+    count = 0
+    for students in student_list:
+        if students.gender == choice:
+            count += 1
+            students.display_name()
+    print(f"\nThere are {count} {choice} students")
 
 
 def generate_students():
@@ -140,36 +194,70 @@ def generate_students():
             Students(line[0], int(line[1]), line[2], line[3], line[4], gender)
 
 
+def menu(class_options, form_classes):
+    new_action = True
+    while new_action:
+
+        print()
+        print("1. Add a student to the system")
+        print("2. Print all student details")
+        print("3. Print details of all students above a particular age")
+        print("4. Count the number of students in a particular subject")
+        print("5. Print details of a particular student")
+        print("6. Print all teacher details")
+        print("7. Print all students of specific gender")
+        print("8. Exit")
+
+        choice = input("\nWhat would you like to do? - Enter a number: ")
+        if choice == "1":
+            add_students(class_options, form_classes)
+
+        elif choice == "2":
+            print_student_details()
+
+        elif choice == "3":
+            select_student_age()
+
+        elif choice == "4":
+            counted_students, teacher = count_students(class_options)
+            if counted_students == 0:
+                print("\nThere are no students in this class")
+            else:
+                print(f"\nThere are {counted_students} students in {teacher}'s"
+                      f" class")
+
+        elif choice == "5":
+            find_student()
+
+        elif choice == "6":
+            print_teacher_details()
+
+        elif choice == "7":
+            print_gender()
+
+        elif choice == "8":
+            print("Goodbye")
+            new_action = False
+
+        else:
+            print("\nPlease choose one of the available options\n")
+
+
 # Main routine
-classes = ["ART", "ENG", "MAT", "GRA", "DTC", "PHY", "BIO"]
-available_form_classes = ["BAKER", "MORGAN", "MCNICOL", "GRAHAM", "BELL",
-                          "NIMMO", "BARKER"]
 student_list = []
+teacher_list = []
+
 generate_students()
 
-new_action = True
-while new_action:
-    print("1. Add a student to the system")
-    print("2. Print all student details")
-    print("3. Print details of all students above a particular age")
-    print("4. Count the number of students in a particular subject")
-    print("5. Print details of a particular student")
-    print("6. Exit")
+Teacher("Baker", "GRA")
+Teacher("Barker", "MAT")
+Teacher("Graham", "BIO")
+Teacher("Morgan", "DTC")
+Teacher("Bell", "PHY")
+Teacher("Nimmo", "ART")
+Teacher("McNicol", "ENG")
 
-    choice = input("\nWhat would you like to do? - Enter a number: ")
-    if choice == "1":
-        add_students(classes, available_form_classes)
-    elif choice == "2":
-        print_student_details()
-    elif choice == "3":
-        select_student_age()
-    elif choice == "4":
-        counted_students = count_students(classes)
-        print(counted_students)
-    elif choice == "5":
-        find_student()
-    elif choice == "6":
-        print("Goodbye")
-        new_action = False
-    else:
-        print("\nPlease choose one of the available options\n")
+classes = get_classes()
+available_form_classes = get_form_classes()
+
+menu(classes, available_form_classes)
